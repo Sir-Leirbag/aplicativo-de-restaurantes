91360828f134e9ecd26e8e0ff076af2bf651dcab
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-07-10 01:24:57 -0300

    Refatorar classe Pessoa: remover lista de pessoas e atributos desnecessários

--- exercicios/livro.py
+++ exercicios/livro.py
@@ -16,31 +16,27 @@
 
 class Pessoa:
 
-    pessoas = []
-
     def __init__(self, nome, idade, profissao):
         self.nome = nome
         self.idade = idade
         self.profissao = profissao
-        self._sindicalizado = False
-        Pessoa.pessoas.append(self)
     
     def __str__(self):
-        return f'nome: {self.nome} | idade: {self.idade} anos | profissão: {self.profissao} | {self._sindicalizado}'
+        return f'nome: {self.nome} | idade: {self.idade} anos | profissão: {self.profissao}'
+    
+    @property
+    def saudacao(self):
+        if self.profissao:
+            return f'Olá, sou {self.nome}, trabalho com {self.profissao}.'
+        else:
+            return f'Olá, sou {self.nome}.'
     
     def aniverario(self):
         self.idade += 1
 
-    def listar_pessoas():
-        for pessoa in Pessoa.pessoas:
-            print(f'{pessoa.nome} | {pessoa.sindicalizado}')
-    
-    @property
-    def sindicalizado(self):
-        return 'v' if self._sindicalizado else 'x'
-
 pessoa1 = Pessoa('Gabriel', 23, 'bancário')
 
-# Pessoa.listar_pessoas()
-for pessoa in Pessoa.pessoas:
-    print(pessoa)
+
+pessoa1.aniverario()
+print(pessoa1)
+print(pessoa1.saudacao)
